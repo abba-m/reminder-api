4c26cf2e5e6acde5456cdc72e50865fc91f3431d
Muhammad Mahmood <mmuhammadabba@gmail.com>
2023-05-31 08:12:03 +0100

    feat: reminder model

---
+++ src/models/reminder.model.js
@@ -0,0 +1,34 @@
+import { sequelizeConn } from "../config/db";
+import { DataTypes } from "sequelize";
+
+export const Reminder = sequelizeConn.define("Reminder", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+
+  description: {
+    type: DataTypes.STRING,
+  },
+
+  date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
